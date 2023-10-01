6b18104b647c254788bd03093afd385baa260365
hansvanstaveren <sater@xs4all.nl>
2023-10-01 21:16:51 +0200

    Slight improvement to windows compile

--- wincompile.bat
+++ wincompile.bat
@@ -1,3 +1,2 @@
-cl /Fe: bigdeal.exe /D NOTX /I . main.c getopt.c collect.c output.c rmd160.c mp.c binomial.c windows.c /link advapi32.lib
+cl /Fe: bigdeal.exe              /I . main.c getopt.c collect.c output.c rmd160.c mp.c binomial.c windows.c /link advapi32.lib
 cl /Fe: bigdealx.exe /D BIGDEALX /I . main.c getopt.c collect.c output.c rmd160.c mp.c binomial.c windows.c /link advapi32.lib
-main
