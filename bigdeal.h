01512b17fc3ad96c7957e1b603ceb4f995dd43d0
hansvanstaveren <sater@xs4all.nl>
2025-04-27 16:41:49 +0200

    First change for 2.1

--- bigdeal.h
+++ bigdeal.h
@@ -3,8 +3,8 @@
  */
 
 #define VERSION_MAJOR	2
-#define VERSION_MINOR	0
-#define VERSION_PAREN	""
+#define VERSION_MINOR	1
+#define VERSION_PAREN	"DEVEL"
 
 #define COMPASS_NORTH	0
 #define COMPASS_EAST	1
